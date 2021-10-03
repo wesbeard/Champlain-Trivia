9f655a1cf18ac813b5a844ad2facec10ced7f707
Wes Beard <wesley.beard@mymail.champlain.edu>
2021-10-03 19:56:53 -0400

    more work on question file

--- app/src/main/java/com/example/champlain_trivia/QuestionActivity.kt
+++ app/src/main/java/com/example/champlain_trivia/QuestionActivity.kt
@@ -3,7 +3,9 @@ package com.example.champlain_trivia
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,6 +15,13 @@ class QuestionActivity : AppCompatActivity() {
 
     private lateinit var promptText: TextView
     private lateinit var promptImage: ImageView
+    private lateinit var submitButton: Button
+    private lateinit var hintButton: Button
+    private lateinit var correctAnswer: RadioButton
+    private lateinit var incorrect1: RadioButton
+    private lateinit var incorrect2: RadioButton
+    private lateinit var incorrect3: RadioButton
+
     var questionNumber = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,29 +29,24 @@ class QuestionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_text_question)
         title = "Question $questionNumber"
 
+        promptText = findViewById(R.id.question_prompt)
+        promptImage = findViewById(R.id.question_image)
+        submitButton = findViewById(R.id.submit)
+        hintButton = findViewById(R.id.hint)
+
+        // * These need to be dynamic *
+        correctAnswer = findViewById(R.id.answer1)
+        incorrect1 = findViewById(R.id.answer2)
+        incorrect2 = findViewById(R.id.answer3)
+        incorrect3 = findViewById(R.id.answer4)
+
         val intent = intent
         val category = intent.getStringExtra("category").toString()
 
         val stream = resources.openRawResource(R.raw.questions)
         val questions = stream.bufferedReader().readText()
 
-        promptText = findViewById(R.id.question_prompt)
-        promptImage = findViewById(R.id.question_image)
-
-        when (category) {
-            "general" -> {
-                promptText.text = category
-            }
-            "buildings" -> {
-                promptText.text = category
-            }
-            "burlington" -> {
-                promptText.text = category
-            }
-            else -> {
-                promptText.text = "category not recognized"
-            }
-        }
+        promptText.text = "> question goes here <"
     }
 
     companion object {
